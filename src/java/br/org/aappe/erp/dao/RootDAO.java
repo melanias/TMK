0cfb8bd268c374b6eb6306a94abcc212317e3600
Phelipe de Oliveira Melanias <phelipe.melanias@gmail.com>
2011-05-07 11:23:57 -0300

    Organização dos imports da classe RootDAO.

--- src/java/br/org/aappe/erp/dao/RootDAO.java
+++ src/java/br/org/aappe/erp/dao/RootDAO.java
@@ -1,10 +1,11 @@
 package br.org.aappe.erp.dao;
 
+import javax.persistence.EntityManager;
+
 import br.com.caelum.vraptor.ioc.Component;
 
 import br.org.aappe.erp.bean.Root;
 import br.org.aappe.erp.repository.RootRepository;
-import javax.persistence.EntityManager;
 
 /**
  * @author Phelipe Melanias
